484ab698fd4efffde4617dd9c5cbe3379a6f363f
20101994aqeel <mianaqeelntu@gmail.com>
2017-01-18 04:48:43 -0800

    Update MainActivity.java

--- app/src/main/java/test/codevilla/co/test/MainActivity.java
+++ app/src/main/java/test/codevilla/co/test/MainActivity.java
@@ -20,22 +20,38 @@ EditText username, password;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         db = new databs(this);
-        final StringBuffer buffer=new StringBuffer();
+      
+        //make the sharedpreference
+        SharedPreferences sharedPreferences=getSharedPreferences("login", Context.MODE_PRIVATE);
+        final SharedPreferences.Editor editor=sharedPreferences.edit();
+
         username=(EditText)findViewById(R.id.username);
         password=(EditText)findViewById(R.id.password);
+
+        // if a user is allready login then it will show the username and password on edit text
+        username.setText(sharedPreferences.getString("name","username"));
+        password.setText(sharedPreferences.getString("password","username"));
+
         login=(Button) findViewById(R.id.log);
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String usname=username.getText().toString();
                 String pass=password.getText().toString();
+             //select all table in the curser
                 cr = db.getinformation();
                 cr.moveToFirst();
                 try {
                     while(cr.moveToNext()) {
                         String u=(cr.getString(cr.getColumnIndex("username")));
                         String p=(cr.getString(cr.getColumnIndex("password")));
+
+                      //compare the editTest username and password with database username and password
+
                         if(usname.equals(u) && pass.equals(p)){
+                            editor.putString("name",usname);
+                            editor.putString("password",pass);
+                            editor.commit();
                             Intent i=new Intent(MainActivity.this,second.class);
                             i.putExtra("message",usname);
                             startActivity(i);
